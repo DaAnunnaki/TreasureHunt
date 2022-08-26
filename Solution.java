ca82e65981a24996a81588d7b38556ac404f1664
DaAnunnaki <93460161+DaAnunnaki@users.noreply.github.com>
2022-08-26 09:01:41 -0500

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,1 +1,27 @@
+import java.util.*;
+
+    public class Solution {
+
+
+        public static void main(String[] args) {
+            
+            Scanner scan = new Scanner(System.in);
+
+            String str = scan.nextLine();
+            int x = 0;
+            String lr;
+            for(int i = 0; i<str.length(); i++) {
+                if(str.substring(i)=="-") {
+                    x++;
+                }
+                if(str.substring(i)=="/" && str.substring(i+1)=="\\") {
+                    lr = "left";
+                }
+                if(str.substring(i)=="\\" && str.substring(i+1)=="/") {
+                    lr = "right";
+                }
+            }
+            scan.close();
+        }
+    }
 
